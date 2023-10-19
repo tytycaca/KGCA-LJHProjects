7d903aee6c09553756b5054bba9c14c2e6fed5ac
jihyuk lee <tytycaca0508@naver.com>
2023-10-19 17:34:40 +0900

    stack, queue 구현

---
+++ CLang_01/Stack.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <Windows.h>
+#define MAX 3
+
+using namespace std;
+
+int cnt, result;
+int arr[MAX];
+
+void init()
+{
+	cnt = -1;
+	result = 0;
+	memset(arr, 0, sizeof(int) * MAX);
+}
+
+void push(int data)
+{
+	if (cnt == MAX)
+	{
+		cout << "overflow!" << endl;
+		return;
+	}
+
+	cnt++;
+	arr[cnt] = data;
+	cout << "PUSH!" << endl;
+
+	return;
+}
+
+void pop()
+{
+	if (cnt == -1)
+	{
+		cout << "underflow!" << endl;
+		return;
+	}
+
+	result = arr[cnt];
+	cnt--;
+	cout << "POP!" << "index: " << cnt+1 << " / data: " << result << endl;
+
+	return;
+}
+
+
+int main()
+{
+	int input = 0;
+	srand(time(NULL));
+
+	init();
+
+	for (int i = 0; i < 10; i++)
+	{
+		input = rand() % 100;
+
+		if (rand() % 3 <= 1)
+			push(input);
+
+		else
+			pop();
+	}
+}
---
+++ CLang_02/Queue.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <Windows.h>
+#define MAX 3
+
+using namespace std;
+
+int cnt, result, in, out;
+int arr[MAX];
+
+void init()
+{
+	cnt = 0;
+	in = 0;
+	out = 0;
+	result = 0;
+	memset(arr, 0, sizeof(int) * MAX);
+}
+
+void push(int data)
+{
+	if (cnt == MAX)
+	{
+		cout << "overflow!" << endl;
+		return;
+	}
+
+	arr[in] = data;
+	cout << "PUSH!" << endl;
+
+	in = ++in % MAX;
+	cnt++;
+
+	return;
+}
+
+void pop()
+{
+	if (cnt == 0)
+	{
+		cout << "underflow!" << endl;
+		return;
+	}
+
+	result = arr[out];
+	cout << "POP!" << "index: " << out << " / data: " << result << endl;
+
+	out = ++out % MAX;
+	cnt--;
+
+	return;
+}
+
+
+int main()
+{
+	int input = 0;
+	srand(time(NULL));
+
+	init();
+
+	for (int i = 0; i < 10; i++)
+	{
+		input = rand() % 100;
+
+		if (rand() % 3 <= 1)
+			push(input);
+
+		else
+			pop();
+	}
+}
